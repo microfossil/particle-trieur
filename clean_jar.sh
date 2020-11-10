ecdef6a7fb1e80b6a628ecb7288b3433b0a381d5
Ross Marchant <ross.marchant@my.jcu.edu.au>
2020-11-10 20:31:51 +1000

    Ignore target directory

--- clean_jar.sh
+++ clean_jar.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-zip -d out/artifacts/ParticleTrieur_jar/ParticleTrieur.jar 'META-INF/*.RSA'
-#zip -dv out/artifacts/ParticleTrieur_jar/ParticleTrieur.jar 'META-INF/*.SF' 'META-INF/*.RSA'
-zip -d target/ParticleTrieur-2.2.0.jar 'META-INF/*.SF' 'META-INF/*.RSA'
+zip -d target/ParticleTrieur.jar 'META-INF/*.SF' 'META-INF/*.RSA'
