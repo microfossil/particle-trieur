ba8794b091be7a93720ea796d1364a30217f81dd
Ross Marchant <ross.g.marchant@gmail.com>
2022-11-30 23:46:01 +1100

    Merge onnx into dev

--- src/main/java/particletrieur/App.java
+++ src/main/java/particletrieur/App.java
@@ -35,7 +35,7 @@ import javax.swing.*;
  */
 public class App extends Application {
 
-    public static String VERSION = "2.4.15";
+    public static String VERSION = "2.4.16";
 
     private static App instance;
     public static App getInstance() {
