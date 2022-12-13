68863240cf4aa276312abd559d04b3950ef40da9
Ross Marchant <ross.g.marchant@gmail.com>
2022-12-14 09:36:10 +1100

    Fix execute flowcam segmenter

--- src/main/java/particletrieur/App.java
+++ src/main/java/particletrieur/App.java
@@ -35,7 +35,7 @@ import javax.swing.*;
  */
 public class App extends Application {
 
-    public static String VERSION = "2.4.16";
+    public static String VERSION = "2.4.17";
 
     private static App instance;
     public static App getInstance() {
