b649ca0ba8d40a541356bbc39a72cffeaae7e0c8
Ross Marchant <ross.g.marchant@gmail.com>
2024-07-21 16:32:31 +1000

    Fix nvidia smi error

--- src/main/java/particletrieur/App.java
+++ src/main/java/particletrieur/App.java
@@ -35,7 +35,7 @@ import javax.swing.*;
  */
 public class App extends Application {
 
-    public static String VERSION = "3.0.5b";
+    public static String VERSION = "3.0.5c";
 
     private static App instance;
     public static App getInstance() {
